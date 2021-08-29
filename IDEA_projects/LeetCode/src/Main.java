e84e6148e58b8388695f4076118dcd47ab84f876
bagavievbulat <buba1219@yandex.ru>
2021-08-29 23:01:36 +0300

    oh shit here we go again

---
+++ IDEA_projects/LeetCode/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    static int[] nums = new int[] {2,7,11,15};
+    static int target = 9;
+
+    public static void twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.println("1");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        twoSum(nums, target);
+    }
+
+}
