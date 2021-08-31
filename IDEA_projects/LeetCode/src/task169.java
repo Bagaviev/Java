f2befea2c5653c78479bd084e95b6b51f645ab32
bagavievbulat <buba1219@yandex.ru>
2021-08-31 20:57:55 +0300

    new leetcode portion

---
+++ IDEA_projects/LeetCode/src/task169.java
@@ -0,0 +1,41 @@
+
+// https://leetcode.com/problems/majority-element/
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class task169 {
+    public int majorityElement(int[] nums) {
+        LinkedHashMap<Integer, Integer> map = (LinkedHashMap<Integer, Integer>) sort(init(nums));
+        System.out.println(map);
+        List<Map.Entry<Integer,Integer>> entryList = new ArrayList<>(map.entrySet());
+        Map.Entry<Integer, Integer> lastEntry = entryList.get(entryList.size() - 1);
+        return lastEntry.getKey();
+    }
+
+    public static Map<Integer, Integer> init(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int cnt = 1;
+            if (map.get(nums[i]) == null)
+                map.put(nums[i], cnt);
+            else
+                map.put(nums[i], map.get(nums[i]) + 1);
+        }
+        return map;
+    }
+
+    public Map<Integer, Integer> sort(Map<Integer, Integer> map) {        // sort Map by value
+        Map<Integer, Integer> sortedMap = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return sortedMap;
+    }
+
+    public static void main(String[] args) {
+        task169 solution = new task169();
+        int[] nums = new int[] {2,2,1,1,1,2,2,3,3,3,3,3,3};
+        int result = solution.majorityElement(nums);
+        System.out.println(result);
+    }
+}
---
+++ IDEA_projects/LeetCode/src/task172.java
@@ -0,0 +1,100 @@
+
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+
+public class task172 {
+    public int trailingZeroes(int n) {  // какая-то магия, индус порешал опять все...
+        int count = 0;
+
+        while (n > 0) {
+            n /= 5;         // неочевидно ни ку я
+            count += n;
+        }
+
+        return count;
+    }
+
+    public static long factorial(int n) {    // сам, без подсказок!
+        if (n == 0 || n < 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        task172 obj = new task172();
+        System.out.println(obj.trailingZeroes(30));
+    }
+}
+
+/*
+ public int trailingZeroes(int n) {      // not funny, didn't laugh
+        int cnt = 0;
+        BigInteger result = factorial(n);
+        System.out.println(result.toString());
+        while (result.mod(BigInteger.valueOf(10)).equals(BigInteger.valueOf(0))) {
+            result = result.divide(BigInteger.valueOf(10));
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        BigInteger m = BigInteger.valueOf(n);
+
+        while (!m.equals(BigInteger.ZERO)) {
+            result = result.multiply(m);
+            m = m.subtract(BigInteger.ONE);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        task172 obj = new task172();
+        System.out.println(obj.trailingZeroes(30));
+    }
+ */
+
+/*
+public class task172 {
+    public int trailingZeroes(int n) {
+        int cnt = 0;
+        BigInteger result = factorial(n);
+        while (result.mod(BigInteger.valueOf(10)).equals(BigInteger.valueOf(0))) {    // збс, четко...
+            result = result.divide(BigInteger.valueOf(10));         // BigInteger вынужденный костыль, чтобы прожевать n > 30
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public static BigInteger factorial(int n) {    // сам, без подсказок!
+        if (n == 0 || n < 0)
+            return new BigInteger(String.valueOf(1));
+        return BigInteger.valueOf(n).multiply(factorial(n - 1));
+    }
+
+    public static void main(String[] args) {
+        task172 obj = new task172();
+        System.out.println(obj.trailingZeroes(30));
+    }
+}
+ */
+
+/*
+class Solution {
+     public int trailingZeroes(int n) {
+        int cnt = 0;
+        long result = factorial(n);
+        while (result % 10 == 0) {    // збс, четко...
+            result /= 10;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public static long factorial(int n) {    // сам, без подсказок!
+        if (n == 0 || n < 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+}
+ */
