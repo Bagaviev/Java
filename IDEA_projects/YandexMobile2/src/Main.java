37a564ba271a4046a1bddfbadb99dc3b5f7ee80a
bagavievbulat <buba1219@yandex.ru>
2022-07-08 00:36:46 +0300

    вне тайминга вторая задача была на ревью странного кода (текстовая)

---
+++ IDEA_projects/YandexMobile2/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] sizeInput = sc.nextLine().split(" ");
+
+        System.out.println(convertInput(sizeInput[0]).equals(convertInput(sizeInput[1])));
+
+
+    }
+
+    private static String convertInput(String str1) {
+        Stack<Character> chars = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != '.')
+                chars.push(str1.charAt(i));
+            else if (!chars.isEmpty())
+                chars.pop();
+        }
+
+        for (char c: chars) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
