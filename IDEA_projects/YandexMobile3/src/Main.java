af233e061a69c6581e9261c6dc7385359f239eeb
bagavievbulat <buba1219@yandex.ru>
2022-11-15 19:52:17 +0300

    отправлено

---
+++ IDEA_projects/YandexMobile3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    // тут были прошлые задачи из прошлых этапов + более легкая задача на ревью кода загружатора температуры :)
+    }
+}
